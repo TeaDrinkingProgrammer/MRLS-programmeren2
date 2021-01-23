e91d4dc67f77c33d9545df07179787188474b34c
Mo-Gal <mo_@live.nl>
2021-01-23 18:16:23 +0100

    Added functioning create method to StudentLogic

--- CCStatisticsApp/src/main/java/CCStatistics/Main.java
+++ CCStatisticsApp/src/main/java/CCStatistics/Main.java
@@ -1,13 +1,20 @@
 
 package CCStatistics;
 import javafx.application.Application;
+import CCStatistics.Domain.Student;
 import CCStatistics.GUI.CourseInteresting;
-
+import CCStatistics.Logic.StudentLogic;
 
 public class Main {    
     public static void main(String[] args) {
-        Application.launch(CourseInteresting.class);
+        //Application.launch(CourseInteresting.class);
         // CertificateDAO newOne = new CertificateDAO();
         // newOne.createCertificate(7.5, "Jan");
+        StudentLogic logic = new StudentLogic();
+
+        Student student = new Student("email@live.com", "firstName", "lastName", "01-01-2020", "M", "street", "Number", "city", "country", "4827GC");
+        logic.create(student);
+
+        
     }
 }
